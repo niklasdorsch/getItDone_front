6cb17f874b67d283ef3d9530e7ddf61ebf8e163d
Niklas <dorschniklas@gmail.com>
2018-05-21 11:41:53 -0400

    ported skeleton

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SampleComponent from './SampleComponent';
+import LoginComponent from './LoginComponent';
+
+const App = () => (
+    <div>
+        App goes heeeeree!
+        <SampleComponent />
+        <LoginComponent />
+    </div>
+);
+
+export default App;
